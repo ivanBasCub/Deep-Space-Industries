a9bf62d0158cb400ffd9612848a3686d2f7b48e4
Ivan Bascones <ibascub2017@hotmail.com>
2026-01-02 19:38:25 +0100

    Configuración Inicial

---
+++ settings/celery.py
@@ -0,0 +1,14 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.settings')
+app = Celery('settings')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    
+}
+
+app.conf.timezone = 'Europe/Madrid'
