e67dbfce566c4db324036d7c63f183f6a65648dd
Ivan Bascones <ibascub2017@hotmail.com>
2026-02-06 11:44:26 +0100

    Modificaciones tarea corp_assets

--- warehouse/tasks.py
+++ warehouse/tasks.py
@@ -14,47 +14,33 @@ def update_corp_asset():
     data = corp_assets(manager)
 
     try:
-        station_items = []
-        seen_ids = set()
-        
-        for item in data:
-            if (
-                item["location_type"] == "item"
-                and item["type_id"] in containers_id
-                and item["item_id"] not in seen_ids
-                and item["location_flag"] in loc_flag_filter
-            ):
-
-                station_items.append({
-                    "item_id": item["item_id"],
-                    "location_flag": item["location_flag"],
-                    "location_id": item["location_id"],  # station_id
-                })
-                seen_ids.add(item["item_id"])
 
         items_by_id = {i["item_id"]: i for i in data}
 
-        def resolve_station(item):
-            loc_flag = item["location_flag"]
-            current = item
+        station_items = {
+            item["item_id"]: {
+                "item_id": item["item_id"],
+                "location_flag": item["location_flag"],
+                "location_id": item["location_id"], 
+            }
+            for item in data
+            if item["location_type"] == "station"
+            and item["type_id"] in containers_id
+            and item["location_flag"] in loc_flag_filter
+        }
 
+        def resolve_station(item):
+            current = item
             while current and current.get("location_type") == "item":
                 parent_id = current["location_id"]
 
-                container = next(
-                    (c for c in station_items if c["item_id"] == parent_id),
-                    None
-                )
+                container = station_items.get(parent_id)
                 if container:
                     return container["location_id"], container["location_flag"]
-
                 current = items_by_id.get(parent_id)
-
-            if current and current.get("location_type") == "station":
-                return current["location_id"], loc_flag
-
+            if current and current.get("location_type") in {"station", "structure"}:
+                return current["location_id"], current.get("location_flag")
             return None, None
-
         
         for item in data:
             if item["location_type"] in loc_filter and item["type_id"] not in containers_id and item["location_flag"] in loc_flag_filter:
@@ -66,15 +52,20 @@ def update_corp_asset():
                 item_id = item["type_id"]
                 item_name = item_data_id(item_id)["name"]
 
-                station_name = structure_data(
-                    character=manager,
-                    structure_id=structure_id
-                )["name"]
+                if resolve_station(item)[1] == "station":
+                    station_name = structure_data(
+                        character=manager,
+                        structure_id=structure_id
+                    )["name"]
+                else:
+                    station_name = structure_data(
+                        character=manager,
+                        structure_id=structure_id
+                    )["name"]
 
-                if "is_blueprint_copy" in item:
-                    if item["is_blueprint_copy"]:
-                        item_name = item_name + " (Copy)"
-                
+                if "is_blueprint_copy" in item and item["is_blueprint_copy"]:
+                    item_name += " (Copy)"
+
                 asset, created = CorpItem.objects.get_or_create(
                     eve_id=item_id,
                     name=item_name,
